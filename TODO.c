5ffd536b38821e7db55673c0f934fcee0334e647
KUR <rhdnfka94@gmail.com>
2017-01-17 16:29:31 +0900

    [code it] list must be coded later! about zombies and job list test

--- TODO.c
+++ TODO.c
@@ -7,8 +7,11 @@ all test passed
 //test2
 	//tsh need to deal with 'quit' cmd
 test3
-	do foreground job - how to test?
+	//do foreground job - how to test?
+	is it ok with bg jobs?	need to implement bg & fg 
 test4
+	impl bg jobs.
+	but how to print ref's output? just use printf???
 test5
 test6
 test7
@@ -21,3 +24,13 @@ test13
 test14
 test15
 test16
+
+how to check child process has been reaped?? -> code it!
+	maybe use waitpid w/ WNOHANG : 이전에 defunct이던 pid를 넣으면
+	그 pid가 반환된다 - 이것으로 zombie인지 테스트할수도 있다...
+	system("ps")
+
+
+maybe I can check job list.. to test something -> code it!
+
+* can you print test output in color?
